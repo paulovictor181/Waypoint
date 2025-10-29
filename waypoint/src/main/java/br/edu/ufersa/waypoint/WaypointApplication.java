78869c6be08f723154f4e63f6a3d404278dede7d
paulovictor181 <paulo_victor_cc82@outlook.com>
2025-10-29 08:26:33 -0300

    first commit

---
+++ waypoint/src/main/java/br/edu/ufersa/waypoint/WaypointApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.waypoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaypointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaypointApplication.class, args);
+	}
+
+}

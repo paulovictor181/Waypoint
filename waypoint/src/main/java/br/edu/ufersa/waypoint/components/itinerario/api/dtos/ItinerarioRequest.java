01f90d7bf61911b1bc1d611b40cecea8c4edec22
paulovictor181 <paulo_victor_cc82@outlook.com>
2025-12-11 00:31:01 -0300

    Ajustando tela de itinerario

---
+++ waypoint/src/main/java/br/edu/ufersa/waypoint/components/itinerario/api/dtos/ItinerarioRequest.java
@@ -0,0 +1,4 @@
+package br.edu.ufersa.waypoint.components.itinerario.api.dtos;
+
+public record ItinerarioRequest() {
+}
---
+++ waypoint/src/main/java/br/edu/ufersa/waypoint/components/usuario/api/restControllers/UsuarioController.java
@@ -0,0 +1,4 @@
+package br.edu.ufersa.waypoint.components.usuario.api.restControllers;
+
+public class UsuarioController {
+}
